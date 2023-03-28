687039d41d3917149740117338f50c25cce6664c
qimushaw <129198895+qimushaw@users.noreply.github.com>
2023-03-28 10:32:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,56 @@
-from telegram.ext import Updater, MessageHandler, Filters
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import openai
+import os
 
-# Define a function to handle incoming messages
+# Set up OpenAI API key
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+# Define function to generate response using GPT
+def generate_response(text):
+    prompt = f"User: {text}\nAI:"
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=50,
+        n=1,
+        stop=None,
+        timeout=20,
+    )
+    message = response.choices[0].text.strip()
+    return message
+
+# Define function to handle user messages
 def handle_message(update, context):
-    message = update.message.text
-    # Your code to process the message goes here
-    # Example code to send a reply message
-    update.message.reply_text('Thanks for your message!')
+    text = update.message.text
+    response = generate_response(text)
+    update.message.reply_text(response)
 
+# Define main function to start the bot
 def main():
-    # Set up the Telegram bot
-    updater = Updater('5955950302:AAFL_RVjydcb1t3IoZTWKJog8aO9ZndxfBE', use_context=True)
+    # Set up Telegram bot and updater
+    token = os.environ['TELEGRAM_BOT_TOKEN']
+    updater = Updater(token, use_context=True)
     dispatcher = updater.dispatcher
-    # Set up a message handler to handle incoming messages
-    message_handler = MessageHandler(Filters.text, handle_message)
+
+    # Define handlers for commands and messages
+    start_handler = CommandHandler('start', start)
+    message_handler = MessageHandler(Filters.text & (~Filters.command), handle_message)
+
+    # Add handlers to dispatcher
+    dispatcher.add_handler(start_handler)
     dispatcher.add_handler(message_handler)
+
     # Start the bot
     updater.start_polling()
+
+    # Run the bot until interrupted
     updater.idle()
 
-if __name__ == '__main__':
+# Define function to handle /start command
+def start(update, context):
+    update.message.reply_text("Hi! I'm a GPT-based chatbot. How can I assist you today?")
+
+if name == 'main':
     main()
